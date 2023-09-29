01c66dd5ffd838dd288e3962b7f60498cd277ee0
Ankush Dongre <dongreankush52@gmail.com>
2023-09-30 01:24:09 +0530

    Project Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,36 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
+import { useAuthContext } from "./hooks/useAuthContext";
+
 function App() {
-  return <div className="App">
-    <BrowserRouter>
-    <Navbar/>
-    <div className="pages">
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-      </Routes>
+  const { user } = useAuthContext();
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <HomePage /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-    </BrowserRouter>
-  </div>;
+  );
 }
 
 export default App;
